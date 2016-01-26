be0802f80ef8782ca18c4eb39f57e0558a0a1423
max9xs <call.max17@gmail.com>
2016-01-26 23:42:32 +0530

    Initial Commit

---
+++ M9-HTML-Editor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  M9-HTML-Editor
+//
+//  Created by Hardik on 26/01/16.
+//  Copyright © 2016 Hardik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
